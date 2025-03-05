1cf8ed48ab8e01c96f892d083597ce699f8e1580
Mathdasc Laboratory <161944774+mathdasc@users.noreply.github.com>
2025-03-05 12:16:09 +0700

    Add files via upload

---
+++ ganjilgenap.py
@@ -0,0 +1,10 @@
+x = float(input("masukkan nilai : "))
+if (x > 3.5):
+    print("index A")
+elif (x > 3):
+    print("index B")
+
+# jika modulo = 0 maka
+#     x adalah genap
+# jika tidak, maka :
+#     x adalah ganjil
